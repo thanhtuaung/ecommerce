e1b1b450576cc9725a92b8df8ab568a25fcc2dd0
thanhtuaung <thanhtooaung.dev@gmail.com>
2023-09-08 20:50:52 +0630

    update cart

--- store/models.py
+++ store/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.user.username
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
+    
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, blank=True, null=True, on_delete=models.SET_NULL)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transition = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return str(self.id)
+    
+    @property
+    def shipping(self):
+        shipping = False
+        order_items = self.orderitem_set.all()
+        for item in order_items:
+            if item.product.digital == False:
+                shipping = True
+                break
+        return shipping
+    
+    @property
+    def items_count(self):
+        items = self.orderitem_set.all()
+        count = sum([item.quantity for item in items])
+        return count
+    
+    @property
+    def get_total(self):
+        items = self.orderitem_set.all()
+        total = sum([item.get_total for item in items])
+        return total
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField(default=0)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.product.name
+    
+    @property
+    def get_total(self):
+        return self.product.price * self.quantity
--- store/views.py
+++ store/views.py
@@ -1,10 +1,74 @@
 from django.shortcuts import render
+from .models import Product, Order, OrderItem
+
+from django.http import JsonResponse
+import json
 
 def store(request):
-    return render(request, 'store/store.html')
+    products = Product.objects.all()
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items_count = order.items_count
+    else:
+        items_count = 0
+
+    context = {'products': products, 'items_count': items_count}
+    return render(request, 'store/store.html', context)
 
 def cart(request):
-    return render(request, 'store/cart.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        items_count = order.items_count
+    else:
+        items = []
+        order = {'items_count': 0, 'get_total': 0}
+        items_count = 0
+        
+    context = {'items': items, 'order': order, 'items_count': items_count}
+    return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    return render(request, 'store/checkout.html')
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        items_count = order.items_count
+        # shipping = order.shipping
+    else:
+        items = []
+        order = {'items_count': 0, 'get_total': 0}
+        items_count = 0
+        # shipping = False
+
+    context = {'items': items, 'order': order, 'items_count': items_count,}  
+    return render(request, 'store/checkout.html', context)
+
+def update_item(request):
+
+    data = json.loads(request.body)
+
+    product_id = data.get('product')
+    action = data.get('action')
+
+    product = Product.objects.get(id=product_id)
+    customer = request.user.customer
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
+    
+    if action == 'add':
+        order_item.quantity = (order_item.quantity + 1)
+    elif action == 'remove':
+        order_item.quantity = (order_item.quantity - 1)
+
+    order_item.save()
+
+    if order_item.quantity <= 0:
+        order_item.delete()
+
+    return JsonResponse('Item was added', safe=False)
